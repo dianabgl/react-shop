0d95ec8c92841ca8238a37660cae7603141f9683
dianabgl <dianabglmv@gmail.com>
2022-08-17 13:58:16 -0500

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './routes/App';
-
-//qué componente va a renderizar y en qué elemento
-ReactDom.render(<App />, document.getElementById('app'));
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-  return (
-    <div>Not Found, error 404.</div>
-  )
-}
-
-export default NotFound
