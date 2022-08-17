5284642acc3345e19c1f457ea8560e86f9bf677f
dianabgl <dianabglmv@gmail.com>
2022-08-17 12:14:11 -0500

    Change public path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'react-shop/bundle.js',
+        filename: 'bundle.js',
         publicPath: './'
     },
     mode: 'development',
