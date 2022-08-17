87dfa0b4e7799b0367d76c9fbd49dc5ca0e5c399
dianabgl <dianabglmv@gmail.com>
2022-08-17 13:09:39 -0500

    API

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-    <BrowserRouter basename="/react-shop">
+    <HashRouter basename="/react-shop">
       <Layout>
         <Routes>        
           <Route exact path="/" element={<Home />} />
@@ -33,7 +33,7 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>    
-    </BrowserRouter>
+    </HashRouter>
     </AppContext.Provider>
   );
 }
